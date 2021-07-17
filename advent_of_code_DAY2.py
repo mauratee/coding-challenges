89babefcfe4da2fd6f4bcc4656291df7fea7a1cf
mauratee <mauraethompson@gmail.com>
2021-07-17 20:53:14 +0000

    Add function to tokenize input data

--- advent_of_code_DAY2.py
+++ advent_of_code_DAY2.py
@@ -7,12 +7,21 @@
 # print(passwords)
 
 # creates data list without newline characters
-with open("AOC_day2_input.txt") as f:
-    data = f.read().splitlines()
+
 
 # print(data)
 # example item: "9-12 k: qkhxrknkkzpk"
 
+
+def parse_policy_and_pw(policies_and_pws):
+    for policy_and_pw in policies_and_pws:
+        policy_range, policy_char, pw = policy_and_pw.split(" ")
+
+        # (5, 6, 'g', 'rvfgnggjgk')
+        return tuple(int(n) for n in policy_range.split("-")) + (
+            policy_char[0],
+            pw
+        )
 
 
 
@@ -29,5 +38,9 @@ with open("AOC_day2_input.txt") as f:
 
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    with open("AOC_day2_input.txt") as f:
+        data = f.read().splitlines()
+
+    print(parse_policy_and_pw(data))
     
