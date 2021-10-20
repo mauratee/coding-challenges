f0af6e7e94cda9a5af46a7196570c4a71fef7e93
Maura Thompson <thomaura@gmail.com>
2021-10-20 11:40:13 -0400

    Finish HB lemur problem

--- HB_challenges/easy_lemur.py
+++ HB_challenges/easy_lemur.py
@@ -30,6 +30,28 @@ def  lemur(branches):
     assert branches[0] == 0, "First branch must be alive"
     assert branches[-1] == 0, "Last branch must be alive"
 
+    if len(branches) <= 1:
+        return 0
+    
+    if len(branches) <= 3:
+        return 1
+
+    pointer = 0
+    jumps = 0
+
+    while pointer < len(branches):
+        pointer += 2
+        if pointer < len(branches):
+            if branches[pointer] == 0:
+                jumps += 1
+            else:
+                pointer -= 1
+                jumps += 1
+        else:
+            break
+
+    return jumps
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
