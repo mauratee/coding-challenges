4c1c9a6e3e1c9d32239e1e56cceb25da6b345496
Maura Thompson <thomaura@gmail.com>
2021-11-04 11:42:06 -0400

    Add blank HB largest sum problem

--- HB_challenges/medium_encipher-string.py
+++ HB_challenges/medium_encipher-string.py
@@ -20,6 +20,23 @@ It should preserve capitalization, whitespace, and any special characters:
 
 def rot_encode(shift, txt):
     """Encode `txt` by shifting its characters to the right."""
+    alpha_dict = {chr(i+96):i for i in range(1,27)}
+    # alpha_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 
+    # 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 
+    # 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 
+    # 'x': 24, 'y': 25, 'z': 26}
+
+    dict_keys = alpha_dict.keys()
+
+    new_text = []
+
+    for letter in txt:
+        value = alpha_dict[letter]
+        new_val = value + shift
+        new_letter = dict_keys[new_val -1]
+        new_text.append(new_letter)
+
+    return "".join(new_text)
 
 
 if __name__ == '__main__':
